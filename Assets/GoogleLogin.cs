6e0677f14746f37c804b0e1a524fdce8139a1a67
LeeYunSeon <ttok14@bluegames.co.kr>
2019-05-30 16:18:30 +0900

    hola

---
+++ Assets/GoogleLogin.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GooglePlayGames;
+using GooglePlayGames.BasicApi;
+
+public class GoogleLogin : MonoBehaviour
+{
+    public string authState = "Not Tried";
+    bool auth;
+    
+    // Use this for initialization
+    void Start()
+    {
+        PlayGamesClientConfiguration config = new PlayGamesClientConfiguration.Builder().Build();
+            // requests an ID token be generated.  This OAuth token can be used to
+            //  identify the player to other services such as Firebase.
+         //   .RequestIdToken()
+         //   .Build();
+
+        PlayGamesPlatform.InitializeInstance(config);
+
+        // recommended for debugging 
+        PlayGamesPlatform.DebugLogEnabled = true;
+
+        // Activate the Google Play Games platform
+        PlayGamesPlatform.Activate();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void SingUpOrLogin()
+    { 
+        Social.localUser.Authenticate(success =>
+        {
+            auth = success;
+
+            authState = auth ? "Success Auth" : "Fail Auth";
+
+            UIUpdator.UpdateUI();
+        });
+    }
+
+    public void SignOut()
+    {
+        PlayGamesPlatform.Instance.SignOut();
+        authState = "SignOut";
+        UIUpdator.UpdateUI();
+    }
+}
---
+++ Assets/UIUpdator.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIUpdator : MonoBehaviour {
+    static bool update;
+
+    public Text fireBaseAuthState;
+    public Text googleLoginAuthState;
+
+    Auth auth;
+    GoogleLogin googleLogin;
+
+    private void Awake()
+    {
+        auth = GetComponent<Auth>();
+        googleLogin = GetComponent<GoogleLogin>();
+    }
+
+    void Update()
+    {
+        if(update)
+        {
+            update = false;
+
+            googleLoginAuthState.text = googleLogin.authState;
+        }
+    }
+
+    static public void UpdateUI()
+    {
+        update = true;
+    }
+}
